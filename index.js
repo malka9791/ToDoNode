1a00ba9eea0b01a5d30f813cda84db1cf03a3402
malka9791 <m0527169791@gmail.com>
2025-02-06 18:01:06 +0200

    first

---
+++ index.js
@@ -0,0 +1,30 @@
+import express from "express";
+import renderApi from "@api/render-api";
+
+// const express = require("express");
+const app = express();
+const port = 3000;
+
+// התחברות ל-Render API
+renderApi.auth("rnd_42NLAwBctESxEP1BBH17XIjY3uZn");
+
+export default app.get("/services", async (req, res) => {
+  try {
+    const { data } = await renderApi.listServices({
+      includePreviews: "true",
+      limit: "20",
+    });
+    res.json(data);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Failed to fetch services" });
+  }
+})
+
+app.get("/", (req, res) => {
+  res.send("Hello!!!");
+});
+
+app.listen(port, () => {
+  console.log(`server runing in port: http://localhost:${port}`);
+});
